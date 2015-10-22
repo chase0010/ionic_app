551890f4360a574385733bdcf10d1cf26fe8ba5e
chase0010 <chase0010@qq.com>
2015-10-22 15:09:24 +0800

    Ionic默认安装后在Android环境下导航从顶部移至底部。

--- www/js/app.js
+++ www/js/app.js
@@ -23,8 +23,19 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
-
+.config(function($stateProvider, $urlRouterProvider,$ionicConfigProvider) {
+	// ,$ionicConfigProvider及下段代码解决ionic默认安装后Android导航在底部
+	$ionicConfigProvider.platform.ios.tabs.style('standard'); 
+	$ionicConfigProvider.platform.ios.tabs.position('bottom');
+	$ionicConfigProvider.platform.android.tabs.style('standard');
+	$ionicConfigProvider.platform.android.tabs.position('standard');
+	$ionicConfigProvider.platform.ios.navBar.alignTitle('center'); 
+	$ionicConfigProvider.platform.android.navBar.alignTitle('left');
+	$ionicConfigProvider.platform.ios.backButton.previousTitleText('').icon('ion-ios-arrow-thin-left');
+	$ionicConfigProvider.platform.android.backButton.previousTitleText('').icon('ion-android-arrow-back');        
+	$ionicConfigProvider.platform.ios.views.transition('ios'); 
+	$ionicConfigProvider.platform.android.views.transition('android');
+	
   // Ionic uses AngularUI Router which uses the concept of states
   // Learn more here: https://github.com/angular-ui/ui-router
   // Set up the various states which the app can be in.
